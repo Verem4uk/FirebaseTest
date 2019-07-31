9759cda2b4322ef052e4835edb889ac6dc261891
Verem4uk <seregaverem4uk@gmail.com>
2019-07-31 13:24:08 +0300

    separation scripts

---
+++ Assets/FirebaseChat.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+
+
+public class FirebaseChat : MonoBehaviour
+{
+    [SerializeField]
+    InterLayer interLayer;
+
+    DatabaseReference dataRef;
+    bool firstRequest = true;
+    
+    struct Message
+    {
+        public string name;
+        public string text;
+
+        public Message(string name, string text)
+        {
+            this.name = name;
+            this.text = text;
+        }
+    }
+
+    private void Start()
+    {
+        ConnectToDB();
+    }
+
+    public void ConnectToDB()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://testchat-b3f0a.firebaseio.com/");
+        dataRef = FirebaseDatabase.DefaultInstance.GetReference("messages");
+        dataRef.ValueChanged += HandleValueChanged;
+    }
+    
+    public void SendMessage(string myMessage, string myName)
+    {
+        Message message = new Message(myName, myMessage);
+        string toSend = JsonUtility.ToJson(message);
+        dataRef.Push().SetRawJsonValueAsync(toSend);
+    }
+
+    void HandleValueChanged(object sender, ValueChangedEventArgs args)
+    {       
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+       
+        string receive = args.Snapshot.GetRawJsonValue();   
+        if(!string.IsNullOrEmpty(receive)) PrepairMessages(receive);        
+    }
+
+    void PrepairMessages (string Json)
+    {
+        string[] rezult = Json.Split('{', '}');
+        foreach(string str in rezult)
+        {
+            print(str);
+        }
+
+        if (firstRequest)
+        {
+
+            for (int i = GetStartIndex(rezult.Length, 6); i < rezult.Length; i += 2)
+            {
+                if (!string.IsNullOrEmpty(rezult[i]))
+                {
+                    Message message = JsonUtility.FromJson<Message>("{" + rezult[i] + "}");
+                    interLayer.WriteToChat(message.name, message.text);
+                }
+            }
+            firstRequest = false;
+        }
+
+        else
+        {
+            Message message = JsonUtility.FromJson<Message>("{" + rezult[rezult.Length - 3] + "}");
+            interLayer.WriteToChat(message.name, message.text);      //т.к. минимальная длина одного сообщения 4
+        }
+    }
+
+    int GetStartIndex(int length, int countOfMessage)
+    {
+        if (length - 3 > countOfMessage * 2) return length - 1 - (countOfMessage * 2);
+        else return 2;
+    }
+
+
+}
---
+++ Assets/InterLayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterLayer : MonoBehaviour
+{
+    [SerializeField]
+    FirebaseChat firebaseChat;
+
+    [SerializeField]
+    Text mainText;
+
+    [SerializeField]
+    InputField inputText;
+
+    [SerializeField]
+    Text nameText;
+
+    string myMessage;
+    string myName;
+    
+    public void PrepareMessage()
+    {
+        if (string.IsNullOrEmpty(myName)) ChangeMyName();
+        myMessage = inputText.text;
+        if (!string.IsNullOrEmpty(myMessage)) firebaseChat.SendMessage(myMessage, myName);
+        inputText.text = "";
+    }
+
+    public void WriteToChat(string name, string message)
+    {
+        mainText.text += name + ": " + message + "\n\n";
+    }
+
+    public void ChangeMyName()
+    {
+        myName = nameText.text;        
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
